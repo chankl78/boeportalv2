240b4ce99162569cdcf523edec08af6ab125c25d
Denis <denis.migal@code-care.pro>
2019-09-21 19:45:34 +0300

    Make change password button good

--- src-spa/src/pages/dashboard/profile/Profile.vue
+++ src-spa/src/pages/dashboard/profile/Profile.vue
@@ -2,7 +2,9 @@
   <q-page padding>
     <div class="q-pa-md q-gutter-sm">
       <p>PROFILE</p>
-      <router-link :to="{name: 'changePassword'}">Home</router-link>
+      <router-link :to="{name: 'changePassword'}">
+        <q-btn color="primary" label="Change password"/>
+      </router-link>
     </div>
   </q-page>
 </template>
